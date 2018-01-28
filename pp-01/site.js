1340a44a8d07165c878fa53a8c476b3fd2871d80
Randal <rkwok1@hawk.iit.edu>
2018-01-27 21:43:20 -0600

    Add good enough email check

--- pp-01/site.js
+++ pp-01/site.js
@@ -1,16 +1,30 @@
 $('#contact-info').on('submit',
   function(e) {
-    e.preventDefault();
     console.log('The form was submitted.');
     var phone = $('#phone').val();
     var re = /\d{10}/;
+    var email = $('#email').val();
+    var re2 = /.*@.*/
+    var validPhone = false;
+    var validEmail = false;
     if(re.test(phone))
     {
-      $(this).remove();
-      $('body').append('<h1>Thank You For Your Submission</h1>');
+      validPhone = true;
     }
     else{
       $('#phone-input').append('<li id="warning">Please enter a proper phone number</li>');
+    }
+    if(re2.test(email))
+    {
+      validEmail = true;
+    }
+    else{
+      $('#email-input').append('<li id="warning">Please enter a proper e-mail address</li>');
+    }
+    if(validPhone && validEmail){
+      e.preventDefault();
+      $(this).remove();
+      $('body').append('<h1>Thank You For Your Submission</h1>');
     }
   }
 );
