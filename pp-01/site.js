841209e39141c81e107b029557f5823bb9e9650f
Randal <rkwok1@hawk.iit.edu>
2018-01-29 11:49:00 -0600

    Fix multiple warning appends

--- pp-01/site.js
+++ pp-01/site.js
@@ -6,22 +6,28 @@ $('#contact-info').on('submit',
     var re2 = /.*@.*/;
     var validPhone = false;
     var validEmail = false;
+    e.preventDefault();
     if(re.test(phone))
     {
       validPhone = true;
+      $('#phone-warning').remove();
     }
     else{
-      $('#phone-input').append('<li id="warning">Please enter a proper phone number</li>');
+      if(!$('#phone-warning').length) {
+        $('#phone-input').append('<li id="phone-warning">Please enter a proper phone number</li>');
+      }
     }
     if(re2.test(email))
     {
       validEmail = true;
+      $('#email-warning').remove();
     }
     else{
-      $('#email-input').append('<li id="warning">Please enter a proper e-mail address</li>');
+      if(!$('#email-warning').length){
+        $('#email-input').append('<li id="email-warning">Please enter a proper e-mail address</li>');
+      }
     }
     if(validPhone && validEmail){
-      e.preventDefault();
       $(this).remove();
       $('body').append('<h1>Thank You For Your Submission</h1>');
     }
